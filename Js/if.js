358a0e3a65f39c0ecd3272f7eb04f43519eba3fa
Ajaxparmar <ajaxparmar@gmail.com>
2026-07-09 18:23:20 +0530

    Basic JS classes

---
+++ Js/if.js
@@ -0,0 +1,26 @@
+let username = "vijay@gmail.com"
+let password = "1234"
+
+let a = 9, b = 80, c = 140
+
+if (a > b) {
+    if (a > c) {
+        console.log("A is greater then B,C");
+    }
+    else
+        {
+            console.log("C is greater then A,B"); 
+        }
+}
+else {
+    if(b>c)
+    {
+        console.log("B is greater then A,C");
+    }
+    else
+        {
+            console.log("C is greater then A,B");
+        }
+   
+}
+
---
+++ Js/op.js
@@ -0,0 +1,31 @@
+// let a=8
+// let b=20
+//  a+=b*10-2   //a=a%b
+// console.log(a);
+
+// let a='90'
+// let b=20
+// a+='h'*b+30
+// console.log(a);
+// console.log(typeof(a));
+
+// let a=false
+
+let b=90
+let a=90
+console.log("testing one");
+
+console.log(a);
+console.log("testing two");
+let d=90
+console.log(d);
+
+
+
+
+
+
+
+
+
+
