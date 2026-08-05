57bb1588a079527ecd5eac479a28f6ec34dc7a08
Khushi <khushikasana761@gmail.com>
2026-08-05 19:27:40 +0530

    JS operators

---
+++ KHUSHI/JS/operators/index.js
@@ -0,0 +1,128 @@
+// assignment operator
+// let a=90;
+// console.log(a);
+
+// let x=5;
+// let s=90;
+// e=s+x
+// console.log(e);
+
+// JavaScript Addition
+
+
+// JavaScript Multiplication
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// console.log(z);
+
+// JavaScript Arithmetic Operators
+// let a=3;
+// let x = (100+50)*a;
+
+
+// let text1 = "John";
+// let text2 = "Doe";
+// let text3 = text1 + " " + text2;
+// console.log(text3);
+
+// let hello = "hi my name";
+// hello += "i will not tell";
+// console.log(hello);
+
+// const y= 4+6;
+// console.log(y);
+
+// const h = 5+'kell'
+// console.log(h);
+
+// JavaScript Comparison Operators
+// let x = 5;
+// let result = x > 8;
+// console.log(result);
+
+// let text1 = "A";
+// let text2 = "B";
+// let result = text1 < text2;
+// console.log(result);
+
+// Arithmetic Operations
+// let a =100 
+// let x = (100 + 50) * a;
+// console.log(x);
+
+
+// Adding
+// let hi = 55;
+// let ror = 32;
+// let nice = hi + ror;
+// console.log(nice);
+
+
+// Subtracting
+// let x = 5;
+// let y = 2;
+// let z = x - y;
+// console.log(z);
+
+
+// Multiplying
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// console.log(z);
+
+
+// Dividing
+// let x = 5;
+// let y = 2;
+// let z = x / y;
+// console.log(z);
+
+
+// Remainder
+// let x = 5;
+// let y = 2;
+// let z = x % y;
+// console.log(z);
+
+
+// Incrementing
+// let x = 5;
+// x++;
+// let z = x;
+// console.log(z);
+
+
+// Decrementing
+// let x = 5;
+// x--;
+// let z = x;
+// console.log(z);
+
+// Exponentiation
+// let x = 5;
+// let z = x ** 2;
+// console.log(z);
+
+
+// let x = 9;
+// let z = Math.pow(x,2);
+// console.log(z);
+
+
+// Operator Precedence
+// let x = 100 + 50 * 3;
+// console.log(x);
+
+
+
+// let x = (100 + 50) * 3;
+// console.log(x);
+
+// let x = 100 + 50 - 3;
+// console.log(x);
+
+
+let x = 100 / 50 * 3;
+console.log(x);
