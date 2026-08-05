812028cdf44e69b4b78c5a408de2eacdfd6d9838
Khushi <khushikasana761@gmail.com>
2026-08-05 17:29:53 +0530

    JS Data Types

---
+++ KHUSHI/JS/data types/index.js
@@ -0,0 +1,46 @@
+// let user = "firstname"
+// let color ="yellow"
+// console.log(user, color);
+
+// let length = 4;
+// const width = 5;
+// console.log(length, width);
+
+// BigInt
+// let x = 1234567890123456789012345n;
+// let y = BigInt(1234567890123456789012345)
+// console.log(x,y);
+
+// Boolean
+// let x = true;
+// let y = false;
+// console.log(x,y);
+
+// Object
+//const person = {firstName:"John", lastName:"Doe"};
+//console.log(person);
+
+
+// Array object
+//const cars = ["Saab", "Volvo", "BMW"];
+//console.log(cars);
+
+// Date object
+//const date = new Date("2022-03-25");
+//console.log(date);
+
+// Undefined
+//let x;
+//let y;
+//console.log(x,y);
+
+// Null
+// let x = null;
+// let y = null;
+// console.log(x,y);
+
+
+// Symbol
+const x = Symbol();
+const y = Symbol();
+console.log(x,y);
