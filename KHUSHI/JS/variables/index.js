26bb2d15d04c35bc18082671cca26d239f785bdb
Khushi <khushikasana761@gmail.com>
2026-08-05 09:03:01 +0530

    Added JS-variables

---
+++ KHUSHI/JS/variables/index.js
@@ -0,0 +1,74 @@
+// let a = 90;
+// let b = 40;
+// let c = a+b;
+// console.log(a,b,c);
+
+//  const x = 5;
+//  const y = 6;
+//  const z = x + y;
+//  console.log(x,y,z);
+
+
+// let _lastname = "Jhon"
+// let _x =2;
+// let _100 =5;
+// console.log(_lastname, _100, _x);
+
+// const _lastname = "Jhon"
+// const _x =2;
+// const _100 =5;
+// console.log(_lastname, _100, _x);
+
+//  A convention among professional programmers is to start a name with underscore for "private" variables.
+
+// JavaScript also treats a dollar sign as a letter.
+// let $ = "hello world";
+// let $$ = 2;
+// let $mymoney =5;
+// console.log($, $$ , $mymoney);
+
+// const $ = "hello world";
+// const $$ = 2;
+// const $mymoney =5;
+// console.log($, $$ , $mymoney);
+
+// declaring a variable (After the declaration, the variable has no value (technically it is undefined)).
+
+// let Carname;
+// Carname = "toyota";
+// console.log(Carname);
+
+// let carName = "Volvo";
+// console.log(carName);
+
+// const carName = "Volvo";
+// console.log(carName);
+
+// const price1 = 40;
+// const price2 = 1;
+// let total = price1 + price2;
+// console.log(total);
+
+// x = 5;
+// y = 6;
+// z = x + y;
+// console.log(z);
+
+//Using VAR
+// var x = 5;
+// var y = 60;
+// var z = x + y;
+// console.log(z);
+
+// const pi = 3.14;
+// let person = "khushi"
+// let answer = "yes i am"
+// console.log(pi , person, answer);
+
+// let person = "John Doe", carName = "Volvo", price = 200;
+// console.log(person ,carName, price );
+
+// let x = 5 + 2 + 3;
+// let y = "John" + " " + "Doe";
+// let z = 1 +"3"
+// console.log(x,y,z);
